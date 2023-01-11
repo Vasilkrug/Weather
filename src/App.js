8c1a331ca683f4f48b4946e1df89e835c8632b59
vasilkrug <vkruglovec@gmail.com>
2023-01-11 21:08:40 +0300

    add sass and bg img to body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
