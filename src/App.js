a2dcd19c15c4c4946cae759164b3f68b16145508
vasilkrug <vkruglovec@gmail.com>
2023-01-22 18:22:10 +0300

    add component Header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,35 @@ import styles from './App.module.scss';
 function App() {
     return (
         <div className={styles.App}>
-            <h1 className={styles}>
-                Hello world!
-            </h1>
+            <div className={styles.weather_app_container}>
+                <header className={styles.header}>
+                    <div className={styles.header_widgets}>
+                        <button><img src={require('./assets/images/icons/dashboard.png')} alt=""/></button>
+                        <button><img src={require('./assets/images/icons/notification.png')} alt=""/></button>
+                        <div className={styles.user_location}>
+                            <button><img src={require('./assets/images/icons/location.png')} alt=""/>
+                            </button>
+                            <p>Moscow, <span>Russia</span></p>
+                        </div>
+                    </div>
+                    <div className={styles.header_search}>
+                        <input type="text" className={styles.search_input} placeholder={'Search city'}/>
+                    </div>
+                    <div className={styles.user_settings}>
+                        <div className={styles.themes_btns}>
+                            <button>
+                                <img src={require('./assets/images/icons/sun-white.png')} alt=""/>
+                            </button>
+                            <button>
+                                <img src={require('./assets/images/icons/moon-yellow.png')} alt=""/>
+                            </button>
+                        </div>
+                        <div className={styles.user_img}>
+                            Fdf
+                        </div>
+                    </div>
+                </header>
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        bgBody:'#101014',
+        textColor:'#4C4B50',
+        bgButtons:'#1E1E1E'
+      },
+      backgroundImage : {
+        searchIcon : "url(assets/images/icons/search.png)"
+      }
+    },
   },
   plugins: [],
 }
