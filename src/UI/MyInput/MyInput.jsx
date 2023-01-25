bb5a0308d3efb731b18b57d9301211b433fb51fd
vasilkrug <vkruglovec@gmail.com>
2023-01-25 22:38:22 +0300

    add function handler to input

--- src/UI/MyInput/MyInput.jsx
+++ src/UI/MyInput/MyInput.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './MyInput.module.scss';
 
-const MyInput = () => {
+const MyInput = ({inputHandler,fetchToWeatherApi,inputValue}) => {
     return (
         <>
-            <input type="text" className={styles.search_input} placeholder={'Search city'}/>
+            <input
+                onChange={(event) => inputHandler(event)}
+                type="text"
+                className={styles.search_input}
+                placeholder={'Search city'}
+                onKeyDown={(e) => fetchToWeatherApi(e)}
+                value={inputValue}
+            />
         </>
     );
 };
